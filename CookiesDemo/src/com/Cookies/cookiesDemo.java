77bfcc7d59f003fe58044eb30f66f84fe14f6974
Digvijay-19 <anneydigvijay19@gmail.com>
2018-05-07 08:39:01 +0530

    UpdatedcookiesScript

--- CookiesDemo/src/com/Cookies/cookiesDemo.java
+++ CookiesDemo/src/com/Cookies/cookiesDemo.java
@@ -11,7 +11,7 @@ public class cookiesDemo {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "D://Selenium Drivers//chromedriver.exe");
+	    System.setProperty("webdriver.chrome.driver", "D://Selenium Drivers//chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();
 	    driver.get("https://www.flipkart.com/");
 	    Set<Cookie> cookie =  driver.manage().getCookies();
